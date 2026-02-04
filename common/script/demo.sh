5d495882f2c018a039ecdc394b2566234a988214
ibm-cloud-appconfiguration <mdevsrvs@in.ibm.com>
2026-02-04 09:14:42 +0000

    Added new changes in sdk appconfiguration-samples.git
    Signed-off-by: ibm-cloud-appconfiguration <mdevsrvs@in.ibm.com>

--- common/script/demo.sh
+++ common/script/demo.sh
@@ -16,18 +16,18 @@ set -e
 
 
 #---------------------------------Setup base url values-------------------------
-region[1]="https://us-south.apprapp.cloud.ibm.com/apprapp/feature/v1/instances"
-region[2]="https://eu-gb.apprapp.cloud.ibm.com/apprapp/feature/v1/instances"
-region[3]="https://au-syd.apprapp.cloud.ibm.com/apprapp/feature/v1/instances"
-region[4]="https://us-east.apprapp.cloud.ibm.com/apprapp/feature/v1/instances"
+# region[1]="https://us-south.apprapp.cloud.ibm.com/apprapp/feature/v1/instances"
+# region[2]="https://eu-gb.apprapp.cloud.ibm.com/apprapp/feature/v1/instances"
+# region[3]="https://au-syd.apprapp.cloud.ibm.com/apprapp/feature/v1/instances"
+# region[4]="https://us-east.apprapp.cloud.ibm.com/apprapp/feature/v1/instances"
 urlSeparator="/"
 config="config"
 queryParam="?clean=true"
 tokenURL="https://iam.cloud.ibm.com/identity/token"
 
 #---------------------------------Get user input---------------------------------
-printf "\nEnter the region where your App configuration service is created\n1. us-south (Dallas)\n2. eu-gb (London)\n3. au-syd (Sydney)\n4. us-east (Washington DC)\n\n"
-read -p "Enter region number> "  regionIn
+printf "\nEnter the region where your App configuration service is created\n"
+read -p "Enter region> "  region
 printf "\n"
 read -p "Enter apikey: (Obtained from Service credentials tab of your instance): "  apikey
 printf "\n"
@@ -43,26 +43,51 @@ while true; do
     esac
 done
 printf "\nrunning the script ...\n"
-baseURL=${region[${regionIn}]}
+baseURL="https://$region.apprapp.cloud.ibm.com/apprapp/feature/v1/instances"
+printf $baseURL
 importConfigURL="$baseURL$urlSeparator$guid$urlSeparator$config$queryParam"
 tokenResponse=$(curl -s -X POST $tokenURL -H "Content-Type: application/x-www-form-urlencoded" -d 'grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey='"$apikey"'')
 access_token=($((echo $tokenResponse | jq -r '.access_token') | tr -d \'\"))
 
+
+importConfigStatus()
+{
+	importStatusURL="$baseURL$urlSeparator$guid$urlSeparator$config/status/$1?action=import"
+	importStatus=$(curl -s --write-out 'Response:%{http_code}' -X GET $importStatusURL -H "Authorization: Bearer $access_token" -H "Accept: application/json" )
+	importStatusBody=$(echo $importStatus | sed -e 's/Response\:.*//g' | jq .)
+	importStatusCode=$(echo $importStatus | tr -d '\n' | sed -e 's/.*Response://')
+	statusInBody=$(echo "$importStatusBody" | jq -r '.status')
+	if [ $importStatusCode -eq 200 ] && [ "$statusInBody" == "completed" ]; then
+		printf "%b\nSuccess:  Import configurations successful.\n"
+	elif [ $importStatusCode -eq 200 ] && [ "$statusInBody" == "inprogress" ] && [ $2 -gt 0 ];	then
+		printf "\nImport Configuration in Progress. Retries left:$2"
+		sleep 2
+		importConfigStatus "$1" $(($2-1))
+	else
+		printf "%b\n \e[31m Failure : Fetching config status failed with error code $importStatusCode and body \n $importStatusBody \e[39m\n"
+		printf "Re-run the script.\n"
+		exit;
+	fi
+}
+
 importConfigurations() 
 {
-	importStatus=$(curl -s --write-out 'HTTPSTATUS:%{http_code}'  -X POST $importConfigURL -H "Authorization: Bearer $access_token" -H "Content-Type: application/json" --data @./configurations.json )
-	HTTP_BODY=$(echo $importStatus | sed -e 's/HTTPSTATUS\:.*//g' | jq .)
-	HTTP_STATUS=$(echo $importStatus | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+	importConfigRequest=$(curl -s --write-out 'HTTPSTATUS:%{http_code}'  -X POST $importConfigURL -H "Authorization: Bearer $access_token" -H "Content-Type: application/json" --data @./configurations.json )
+	HTTP_BODY=$(echo $importConfigRequest | sed -e 's/HTTPSTATUS\:.*//g' | jq .)
+	HTTP_STATUS=$(echo $importConfigRequest | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
 	printf "%b\nHTTP_STATUS is $HTTP_STATUS"
-	if [ $HTTP_STATUS != 201 ] # check status code
+	if [ $HTTP_STATUS -eq 202 ] # check status code
 	then
+		referenceID=$(echo "$HTTP_BODY" | jq -r '.referenceId')
+		printf "\nReferenceID:$referenceID"
+		importConfigStatus "$referenceID" 5
+	else
 		printf "%b\n \e[31m Failure : Importing configurations failed with error code $HTTP_STATUS and body \n $HTTP_BODY \e[39m\n"
 		printf "Re-run the script.\n"
 		exit;
-	else 
-		printf "%b\nSuccess:  Import configurations successful.\n"
-	fi
+	fi	
 }
+
 
 importConfigurations
 
